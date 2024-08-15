c3fbd058510f9893fcd52054d9bb549c3542b7d8
Hendrik Vrey <vreyhendrik7@gmail.com>
2024-08-15 22:49:08 +0200

    Update BrowserTempFileCleanUp.ps1

--- BrowserTempFileCleanUp.ps1
+++ BrowserTempFileCleanUp.ps1
@@ -1,4 +1,4 @@
-﻿# Function to take ownership and grant full control
+﻿#Function to take ownership and grant full control
 function Grant-FullControl {
     param (
         [string]$path
@@ -11,7 +11,7 @@ function Grant-FullControl {
     }
 }
 
-# Function to clear temporary files
+#Function to clear temporary files
 function Clear-TempFiles {
     param (
         [string]$dataPath
@@ -20,12 +20,12 @@ function Clear-TempFiles {
     if (Test-Path -Path $dataPath) {
         Grant-FullControl -path $dataPath
         
-        # Attempt to remove the files with error handling
+        #Attempt to remove the files with error handling
         Remove-Item -Path $dataPath -Recurse -Force -ErrorAction SilentlyContinue -ErrorVariable removeError
 
         if ($removeError) {
             Write-Output "Failed to remove some items in: $dataPath - $($removeError.Exception.Message)"
-            # Optionally, you could add a retry mechanism here
+            #Optionally, you could add a retry mechanism here
         } else {
             Write-Output "Temporary files cleared successfully from: $dataPath"
         }
@@ -34,9 +34,9 @@ function Clear-TempFiles {
     }
 }
 
-# Clear browser temporary files
+#Clear browser temporary files
 function Clear-BrowserTempFiles {
-    # Clear Internet Explorer temp files
+    #Clear Internet Explorer temp files
     try {
         RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 255
         Write-Output "Internet Explorer temporary files cleared successfully."
@@ -44,18 +44,18 @@ function Clear-BrowserTempFiles {
         Write-Output "Failed to clear Internet Explorer temporary files - $($_.Exception.Message)"
     }
 
-    # Define paths for Edge, Chrome, and Firefox
+    #Define paths for Edge, Chrome, and Firefox
     $edgeDataPath = "$env:LocalAppData\Microsoft\Edge\User Data\Default\Cache"
     $chromeDataPath = "$env:LocalAppData\Google\Chrome\User Data\Default\Cache"
     $firefoxProfilePath = "$env:AppData\Mozilla\Firefox\Profiles"
 
-    # Clear Edge temp files
+    #Clear Edge temp files
     Clear-TempFiles -dataPath $edgeDataPath
 
-    # Clear Chrome temp files
+    #Clear Chrome temp files
     Clear-TempFiles -dataPath $chromeDataPath
 
-    # Clear Firefox temp files
+    #Clear Firefox temp files
     if (Test-Path -Path $firefoxProfilePath) {
         $firefoxCachePaths = Get-ChildItem -Path $firefoxProfilePath -Recurse -Filter "cache2"
         foreach ($cachePath in $firefoxCachePaths) {
@@ -66,7 +66,7 @@ function Clear-BrowserTempFiles {
     }
 }
 
-# Start clearing temp files
+#Start clearing temp files
 Clear-BrowserTempFiles
 
 Write-Output "Temporary internet files cleanup process completed."
